579868be6913b6f3cf2873ce8ed053ccf25ba170
Arlandis Lawrence <arlandislawrence@gmail.com>
2014-10-10 13:54:20 -0500

    Move to new rspec syntax

--- spec/lib/cache_spec.rb
+++ spec/lib/cache_spec.rb
@@ -11,7 +11,7 @@ describe Enamel::Cache do
   it "caches by request" do
     test_env = { "REQUEST_URI" => "/foo" }
     backend_response = [200, {}, "OK"]
-    Enamel::Backend.stub(:response).with(test_env).and_return({ttl: 20, resp: backend_response})
+    allow(Enamel::Backend).to receive(:response).with(test_env).and_return({ttl: 20, resp: backend_response})
     expect(Enamel::Cache.response(test_env)).to eq([200, {"X-Enamel" => "miss"}, "OK"])
   end
 
@@ -19,23 +19,25 @@ describe Enamel::Cache do
     now = Time.now
     test_env = { "REQUEST_URI" => "/foo" }
     backend_response = [200, {}, "OK"]
-    Time.stub(:now).and_return(now)
-    Enamel::Backend.stub(:response).with(test_env).and_return({ttl: 20, resp: backend_response})
+    allow(Time).to receive(:now).and_return(now)
+    allow(Enamel::Backend).to receive(:response).with(test_env).and_return({ttl: 20, resp: backend_response})
     Enamel::Cache.response(test_env)
     response_age = 5
-    Time.stub(:now).and_return(now + response_age)
+    allow(Time).to receive(:now).and_return(now + response_age)
     expect(Enamel::Cache.response(test_env)).to eq([200, {"X-Enamel" => "hit", "X-Enamel-Age" => response_age.to_s}, "OK"])
   end
 
-  xit "doesn't hit the backend if it has cached responses" do
+  it "doesn't hit the backend if it has cached responses" do
     now = Time.now
     test_env = { "REQUEST_URI" => "/foo" }
     backend_response = [200, {}, "OK"]
     ttl = 5
-    expect(Enamel::Backend).to receive(:response).with(test_env).and_return({ttl: ttl, resp: backend_response})
+    allow(Enamel::Backend).to receive(:response).with(test_env).and_return(
+      {ttl: ttl, resp: backend_response}
+    )
     Enamel::Cache.response(test_env)
-    Time.stub(:now).and_return(now + ttl)
-    expect(Enamel::Backend).to receive(:response).exactly(0).times
+    allow(Time).to receive(:now).and_return(now + ttl)
+    expect(Enamel::Backend).not_to receive(:response)
     Enamel::Cache.response(test_env)
   end
 
@@ -43,11 +45,11 @@ describe Enamel::Cache do
     now = Time.now
     test_env = { "REQUEST_URI" => "/foo" }
     backend_response = [200, {}, "New stuff"]
-    Time.stub(:now).and_return(now)
-    Enamel::Backend.stub(:response).with(test_env).and_return({ttl: 5, resp: backend_response})
+    allow(Time).to receive(:now).and_return(now)
+    allow(Enamel::Backend).to receive(:response).with(test_env).and_return({ttl: 5, resp: backend_response})
     Enamel::Cache.response(test_env)
     response_age = 5
-    Time.stub(:now).and_return(now + response_age)
+    allow(Time).to receive(:now).and_return(now + response_age)
     expect(Enamel::Cache.response(test_env)).to eq([200, {"X-Enamel" => "miss"}, "New stuff"])
   end
 
